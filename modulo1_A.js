389070fb7fdc72ed5d11d71f1ca5127d6ea8cd9b
Paulo Henrique Costa <89080540+PauloHenriqCosta@users.noreply.github.com>
2025-04-28 13:50:34 +0000

    anotacoes 'js capaz de fazer'

---
+++ modulo1_A.js
@@ -0,0 +1,5 @@
+//cliente VS server | roda client VS roda servidor
+// jornalista(html/conteúdo) Designer(CSS/layoult) Programador(js*/interação) }client  *é linguagem
+
+// inspecionar > debugger > disable js
+//>alterar js que chegou na máquina
